fb5a3fe97f78c2187f361cf9dc2ea2edae0a03bb
Andreas Wegund <andreas.wegund@omv.com>
2020-11-29 17:16:21 +0100

    init-2
    Signed-off-by: Andreas Wegund <andreas.wegund@omv.com>

---
+++ app.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var https = require('https');
+var express = require('express');
+/*---------------------------------------------------------------------*/
+
+
+var privateKey  = fs.readFileSync(__dirname + '/SSL/sawac.key', 'utf8');
+var certificate = fs.readFileSync(__dirname + '/SSL/sawac.crt', 'utf8');
+var credentials = { 
+                    key:  privateKey, 
+                    cert: certificate
+                  };
+
+
+
+
+var app = express();
+app.get('/', (req, res) => { 'Now using HTTPS!!' });
+
+
+
+var httpsServer = https.createServer(credentials, app);
+
+
+
+
+/*---------------------------------------------------------------------*/
+// Server starten
+httpsServer.listen(8443, () => {
+    console.log('HTTPS-Server up and running!');
+});
