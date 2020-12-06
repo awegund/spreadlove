5e28bb3928ab0e88c556f24e9c8dd363b006b0ad
Andreas Wegund <andreas.wegund@omv.com>
2020-12-06 21:03:21 +0100

    Hintergrundfarbe geändert

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var app = express();
 app.get('/', (req, res) => { 
     res.status(200);
     res.setHeader('Content-Type', 'text/html');
-    res.end(`{ "mesage": "<H1>Nun funktioniert es endlich!</H1>" }`); 
+    res.end("<H1 style='background-color:blue'>Nun funktioniert es endlich!</H1>"); 
 });
 
 
