dbf44b261fd673832cb2437a785eb573c853afae
Andreas Wegund <andreas.wegund@omv.com>
2020-12-08 00:06:07 +0100

    RedisSessionStore

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use( (req, res, next) => {
 });
 
 app.use(session({
-    secret:            'ThisIsTheSpreadLoveSessionCookie',
+    secret:            'ThisIsTheSpreadLoveSessionStore',
     store:              redis,
     resave:             false,
     saveUninitialized:  false,
