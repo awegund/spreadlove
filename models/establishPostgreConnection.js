3554a9107ecda1f365efc8d90b77c4bd3d5b9385
Andreas Wegund <andreas.wegund@omv.com>
2020-12-13 17:02:04 +0100

    SequelizeDB active and running

--- models/establishPostgreConnection.js
+++ models/establishPostgreConnection.js
@@ -2,17 +2,15 @@
 const Sequelize = require('sequelize');
 
 // Option 1: Establish connection via URI
- const sequelize = new Sequelize(process.env.POSTGRES_URL, { dialect: 'postgres' });
-
-
-// // Option 2: Passing parameters separately (other dialects)
-// const sequelize = new Sequelize({
-//     process.env.POSTGRES_DB, 
-//     process.env.POSTGRES_USER, 
-//     process.env.POSTGRES_PWD, {
-//       host: process.env.POSTGRES_HOST,
-//       dialect: 'postgres', 
-//       SSL: true
-// }});
+ const sequelize = new Sequelize(process.env.POSTGRES_URL, { 
+     dialect: 'postgres' , 
+     dialectOptions: { 
+        // native: true, 
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        } 
+        } 
+    });
 
 module.exports = sequelize;
