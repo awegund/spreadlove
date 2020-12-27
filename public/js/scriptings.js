c3e713d1545bd84eae089bfd2fe72dbdbe689c08
Andreas Wegund <andreas.wegund@omv.com>
2020-12-27 19:53:47 +0100

    upload latest version

--- public/js/scriptings.js
+++ public/js/scriptings.js
@@ -1,21 +1,56 @@
 /*-------------------------------------------------------------*
- *             Sonstiges JS                                    *
+ *             NAVBAR EVENT HANDLING                           *
  *-------------------------------------------------------------*/
-window.onload = function() {
-
-    // Scroll Event einstellen
-    $(window).on('scroll', function() {
-        if( $(window).scrollTop() > 50 ){
+// Scroll Event einstellen
+$(window).on('scroll', function() {
+    if( $(window).scrollTop() > 50 ){
+        $('nav.navbar').addClass('bg-white');
+        $('nav.navbar').removeClass('pt-3');
+        $('nav.navbar').removeClass('pb-5');
+        $('#backtotop').css('visibility', 'visible');
+    } else {
+        //Bildanfang und Collapse sichtbar = Hintergrund weiß
+        if($(window).width() <= 992 ){
             $('nav.navbar').addClass('bg-white');
-            $('nav.navbar').removeClass('pt-3');
-            $('nav.navbar').removeClass('pb-5');
         } else {
             $('nav.navbar').removeClass('bg-white');
+        }
+        //Bildanfang und KEIN Collapse Menu = paddings dazu
+        //Bildanfang und Collapse Menu = paddings weg
+        if($(window).width() > 992 ){
             $('nav.navbar').addClass('pt-3');
             $('nav.navbar').addClass('pb-5');
+        } else {
+            $('nav.navbar').removeClass('pt-3');
+            $('nav.navbar').removeClass('pb-5');    
         }
-    });
+        $('#backtotop').css('visibility', 'hidden');
+    }
+});
+
+// Initial Werte versorgen
+window.onload = function() {
+    //Navigation white onLoad
+    if($(window).width() <= 992 ){
+        $('nav.navbar').addClass('bg-white');
+    }
+    //Bildanfang und KEIN Collapse Menu = paddings dazu
+    //Bildanfang und Collapse Menu = paddings weg
+    if($(window).width() > 992 ){
+        $('nav.navbar').addClass('pt-3');
+        $('nav.navbar').addClass('pb-5');
+    } else {
+        $('nav.navbar').removeClass('pt-3');
+        $('nav.navbar').removeClass('pb-5');    
+    }
+    $('#backtotop').css('visibility', 'hidden');
+
+
+    //Initialize animated Scroll libary
+    AOS.init({  duration: 1800 });
 }
+
+
 
 
 /*-------------------------------------------------------------*
